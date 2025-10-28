43fed459a1094567efc665a996381c5323e8ed24
Kamla23 <kamlabai23@navgurukul.org>
2025-10-28 12:09:46 +0530

    added diffrence array solution

---
+++ DSA/Array/rangeupdate.js
@@ -0,0 +1,27 @@
+
+
+let arr = [0,0,0,0,0];
+
+let d = [];
+d[0] = arr[0];
+for(let i = 1; i < arr.length; i++){
+    d[i] = arr[i]- arr[i-1];
+}
+console.log(d);
+
+function update(d, l, r, v){
+        d[l] += v;
+        if(r+1 < d.length){
+            d[r+1] -= v
+        }
+}
+update(d, 1,3,5);
+update(d, 2,4,2);
+console.log(d);
+
+let a = [];
+a[0] = d[0];
+for(let j = 1; j < arr.length; j++){
+    a[j] = a[j-1] + d[j];
+}
+console.log(a);
